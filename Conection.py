4945b496f65e666bc9b9a1978d164031b9218348
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2025-03-13 20:32:20 -0500

    [MEDIUM]: Conection

---
+++ Conection.py
@@ -0,0 +1,32 @@
+# pip install oracledb
+# python Conection.py
+import oracledb  
+
+
+# Conectar a Oracle XE en localhost
+conn = oracledb.connect(
+    user="system",
+    password="12345",
+    dsn="localhost:1521/XEPDB1"  # Reemplaza "XEPDB1" con "XE" si es necesario
+)
+
+
+# Crear cursor y ejecutar una consulta de prueba
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM SYSTEM.CUSTOMER")
+
+
+# Obtener y mostrar los registros
+rows = cursor.fetchall()
+
+
+if rows:
+    for row in rows:
+        print(row)  # Imprime cada fila de la tabla
+else:
+    print("No hay registros en la tabla CUSTOMER.")
+
+
+# Cerrar cursor y conexión
+cursor.close()
+conn.close()
